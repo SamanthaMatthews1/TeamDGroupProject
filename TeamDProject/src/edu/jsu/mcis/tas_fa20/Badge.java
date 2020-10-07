3fb9314bc0be4b98b9c9cddf8b7cc21d185e9c43
Tony <55773397+YoungGripper@users.noreply.github.com>
2020-10-07 12:23:19 -0500

    Update Badge.java

--- TeamDProject/src/edu/jsu/mcis/tas_fa20/Badge.java
+++ TeamDProject/src/edu/jsu/mcis/tas_fa20/Badge.java
@@ -9,11 +9,35 @@ package edu.jsu.mcis.tas_fa20;
  *
  * @author penguin
  */
+
+
 public class Badge {
     
      private String id, name;
     
-    public Badge(String id, String name){
+    
+    // the constructor for all badges
+    public Badge(String id, String name)
+    {
         this.id = id;
         this.name = name;
-}
+    }
+
+    //Making all return statements
+    public String getId()
+    {
+        return this.id;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    
+    //changing the configuration of the badge to a string
+    public String toString()
+    {
+        String s = "#" + id + " (" + name + ")";
+        return s;
+    }
