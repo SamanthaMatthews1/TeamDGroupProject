68c56fb2dea5f50477785d47d4caffe228192328
Kameron McAdams <kmcadams@stu.jsu.edu>
2020-10-07 12:29:52 -0500

    Updated Punch class

--- TeamDProject/src/edu/jsu/mcis/tas_fa20/Punch.java
+++ TeamDProject/src/edu/jsu/mcis/tas_fa20/Punch.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.jsu.mcis.tas_fa20;
 
+import java.util.Date;
+import java.sql.*;
 /**
  *
  * @author penguin
  */
 public class Punch {
     
+    private int terminalid;
+    private int punchtypeid;
+    private Badge badge;
+    private int id;
+    private Date date;
+
+    Punch(int id, int terminalid, Badge badge, Date date, int punchtypeid) {
+       this.id = id;
+       this.terminalid = terminalid;
+       this.badge = badge;
+       this.date = date;
+       this.punchtypeid = punchtypeid;
+    }
+    
+    public int getTerminalId() {
+        return terminalid;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public Badge getBadge() {
+        return badge;
+    }
+    
+    public int getPunchTypeId() {
+        return punchtypeid;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
 }
