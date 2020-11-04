9cb713b4492ad9ffbb9ec33b8ccddd6b70b74b65
Tony <55773397+YoungGripper@users.noreply.github.com>
2020-11-04 12:20:47 -0600

    Update TASLogic.java
    I can't check f4 or f5 without f3 fixed but I believe my code is right!

--- TeamDProject/src/edu/jsu/mcis/tas_fa20/TASLogic.java
+++ TeamDProject/src/edu/jsu/mcis/tas_fa20/TASLogic.java
@@ -18,52 +18,52 @@ import org.json.simple.JSONValue;
 
 public class TASLogic {
     
-    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
-        int total = 0;
-        int day;
-        long inTime = 0;
+   // public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+        //int total = 0;
+       // int day;
+       // long inTime = 0;
 
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(dailypunchlist.get(0).getOriginaltimestamp());
-        day = cal.get(Calendar.DAY_OF_WEEK);
+       // Calendar cal = Calendar.getInstance();
+       // cal.setTimeInMillis(dailypunchlist.get(0).getOriginaltimestamp());
+        //day = cal.get(Calendar.DAY_OF_WEEK);
 
-        for (Punch punch : dailypunchlist) {
-            switch (punch.getPunchtypeid()) {
-                case 0:
-                    int minutes = (int)((punch.getAdjustedtimestamp() - inTime) / 60000);
-                    if (minutes > shift.getLunchDeduct(day)) {
-                        minutes -= shift.getLunchDuration(day);
-                    }
-                    total += minutes;
-                    break;
-                case 1:
-                    inTime = punch.getAdjustedtimestamp();
-                    break;
-                case 2:
-                    break;
-            }
-        }
+        //for (Punch punch : dailypunchlist) {
+          //  switch (punch.getPunchtypeid()) {
+             //   case 0:
+                   // int minutes = (int)((punch.getAdjustedtimestamp() - inTime) / 60000);
+                   // if (minutes > shift.getLunchDeduct(day)) {
+                   //     minutes -= shift.getLunchDuration(day);
+                   // }
+                   // total += minutes;
+                   // break;
+               // case 1:
+                 //   inTime = punch.getAdjustedtimestamp();
+                 //   break;
+               // case 2:
+           //         break;
+          //  }
+      //  }
 
-        return total;
-}
+      //  return total;
+//}
  
     //F5
-public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
-        HashMap<String, String> map;
-        ArrayList<HashMap<String, String>> mapList = new ArrayList<>();
+//public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist){
+        //HashMap<String, String> map;
+       // ArrayList<HashMap<String, String>> mapList = new ArrayList<>();
 
-        for (Punch punch : dailypunchlist) {
-            map = new HashMap<>();
-            map.put("id", String.valueOf(punch.getId()));
-            map.put("terminalid", String.valueOf(punch.getTerminalid()));
-            map.put("punchtypeid", String.valueOf(punch.getPunchtypeid()));
-            map.put("badgeid", punch.getBadgeid());
-            map.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp()));
-            map.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp()));
-            map.put("punchdata", punch.getAdjustmenttype());
-            mapList.add(map);
-        }
+        //for (Punch punch : dailypunchlist) {
+           // map = new HashMap<>();
+           // map.put("id", String.valueOf(punch.getId()));
+           // map.put("terminalid", String.valueOf(punch.getTerminalid()));
+           // map.put("punchtypeid", String.valueOf(punch.getPunchtypeid()));
+           // map.put("badgeid", punch.getBadgeid());
+           // map.put("originaltimestamp", String.valueOf(punch.getOriginaltimestamp()));
+           // map.put("adjustedtimestamp", String.valueOf(punch.getAdjustedtimestamp()));
+           // map.put("punchdata", punch.getAdjustmenttype());
+          //  mapList.add(map);
+       // }
 
-        return JSONValue.toJSONString(mapList);
-    }
+      //  return JSONValue.toJSONString(mapList);
+   // }
 
